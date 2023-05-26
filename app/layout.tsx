17a25e2dbfff79983b9f81cf620766dbfc406295
agiklo <mateuszmilewczyk1@gmail.com>
2023-05-27 01:58:12 +0200

    create navbar without functionalities

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
 import './globals.css'
-import { Nunito } from 'next/font/google'
+import {Nunito} from 'next/font/google'
+import Navbar from "@/app/components/navbar/Navbar";
 
-const font = Nunito({ 
-  subsets: ['latin'] 
+const font = Nunito({
+    subsets: ['latin']
 })
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
+    title: 'Airbnb',
+    description: 'Airbnb clone',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className={font.className}>
+        <Navbar />
+        {children}
+        </body>
+        </html>
+    )
 }
